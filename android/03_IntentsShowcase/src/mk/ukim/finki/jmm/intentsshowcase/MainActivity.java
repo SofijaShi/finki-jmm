c3851aad55ed56c96423c8cbd5e5854e620614f7
ristes <ristes@github.com>
2014-10-17 20:04:31 +0200

    start activity for result added

--- android/03_IntentsShowcase/src/mk/ukim/finki/jmm/intentsshowcase/MainActivity.java
+++ android/03_IntentsShowcase/src/mk/ukim/finki/jmm/intentsshowcase/MainActivity.java
@@ -7,8 +7,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -50,11 +52,12 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(Intent.ACTION_SEND);
 		intent.setType("text/plain");
 		intent.putExtra(android.content.Intent.EXTRA_TEXT, "News for you!");
+		intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject!");
 		startActivity(intent);
 	}
 
 	public void showFinkiHomePage(View view) {
-		String finkiUrl = "http://finki.ukim.mk1";
+		String finkiUrl = "http1://finki.ukim.mk";
 		Intent intent = new Intent(Intent.ACTION_VIEW);
 		intent.setData(Uri.parse(finkiUrl));
 		if (isIntentAvailable(this, intent)) {
@@ -62,6 +65,31 @@ public class MainActivity extends Activity {
 		} else {
 			Toast.makeText(this, "No activity for intent: " + intent,
 					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	/**
+	 * Action for retrieving a picture for the gallery
+	 * 
+	 * @param view
+	 */
+	public void selectPicture(View view) {
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+				1010);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == 1010) {
+				Uri selectedImageUri = data.getData();
+				String selectedImagePath = selectedImageUri.toString();
+				Toast.makeText(this, selectedImagePath, Toast.LENGTH_LONG)
+						.show();
+			}
 		}
 	}
 
